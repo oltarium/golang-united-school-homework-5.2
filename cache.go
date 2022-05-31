7247cf097b9bcabedd9fbbd803ed6186e486c2de
Gleb Kazak <zilian159@gmail.com>
2022-06-01 00:50:45 +0300

    lab-5.2: solution

--- cache.go
+++ cache.go
@@ -2,22 +2,48 @@ package cache
 
 import "time"
 
+type Value struct {
+	value     string
+	exp       time.Time
+	canExpire bool
+}
+
 type Cache struct {
+	cache map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cache: make(map[string]Value),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache Cache) Get(key string) (string, bool) {
+	value, ok := cache.cache[key]
+	if !value.canExpire || (value.canExpire && value.exp.After(time.Now())) {
+		return value.value, ok
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) Put(key, value string) {
+	cache.cache[key] = Value{value: value, canExpire: false}
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Keys() []string {
+	keys := []string{}
+	for key, value := range cache.cache {
+		if !value.canExpire || (value.canExpire && value.exp.After(time.Now())) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) PutTill(key, value string, deadline time.Time) {
+	cache.cache[key] = Value{value: value, exp: deadline, canExpire: true}
+}
+
+func (cache Cache) Clear() {
+	cache.cache = make(map[string]Value)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module lab-5.2
+
+go 1.18
